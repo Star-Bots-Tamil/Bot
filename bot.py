cb6ff5ab8605a9549a0559862987308469c7087e
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-01 18:57:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,19 +16,9 @@ BOT_TOKEN = os.environ.get("TOKEN", "7713324413:AAGYzSbK5Z9KbhaJGiQdA7Rs-YaYLk4f
 API_ID = int(os.environ.get("API_ID", 11973721))
 API_HASH = os.environ.get("API_HASH", "5264bf4663e9159565603522f58d3c18")
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMINS', '1391556668 1242556540 5239847373').split()]
-STRING_SESSION = os.environ.get("STRING_SESSION", "BQAlMzYWr3AFQgzppOcTWe3Mh7VGCKGAnObs6tykVwyexB04GNElsS1o12aRSeTTuGamgNJMssiUls33QEUfTajt27zrUQCi0HQ-qCCI8D_IT6pDnlv2tgCnHwkcEYJ6vUgBxPZSdMkkfnQ4tiL93MA4Vxt2Gwd6FUrxD2qwUXOQRnXUFIBTYPi1ihcjkijBU3-iixEsQwOYISLuM3sJ10liG-CdITfVSAhdIYciegR4-BNIHGmeYs38GMbbhFA0dM0i0z7Orm7FsziOZ_r56YbWbWuOH8R3Re-7hntBrThvXP_nI47zmYxuC8zIZ-qleZiI-VhHRoBCHNA46VWf6xHlUvF4PAA")
+STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOKEBu5Pf_Oesjuxt4TIzNijt1iMjJ8hEa3xtURQFrsd0GFYLhS_XFm2iJ61NfFeKR5icfMSu_SWH3eRvvdZ-X7IyOVFZuQ4sHKoiju_WXCH4uQqqd7vB7_9hGyMbDk7mUgjVKNkRg0trupt-5mu8pAeWAZ3US61kBnLKvsMYSjiaiL3uWI3UDfzyNQzFhf_hXWF_XskD0QrMPS87wEd85iNzXBgBE9Sae2haJ8YppGWxhcGtmJDSqHnDSlxh2dFLBZ1K_o7zxE6i1FrOaqEL_gKW87xqc2W43kCsUj-s9A9GyXdP7aUxu1Mku5j3GyMxEWS79Yku7AfxyeGUYhTw5dXGScE=")
 
 bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
-
-user = ''
-if len(STRING_SESSION) != 0:
-    print("Creating client from USER_SESSION_STRING")
-    try:
-        user = wztgClient('user', API_ID, API_HASH, session_string=STRING_SESSION,
-                        parse_mode=enums.ParseMode.HTML, no_updates=True).start()
-    except Exception as e:
-        print(f"Failed making client from USER_SESSION_STRING : {e}")
-        user = ''
 
 if STRING_SESSION:
     apps = [Client2,bot]
