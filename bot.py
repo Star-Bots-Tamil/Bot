1896067f2d2a82f8ad083a51719cefa5c39e02c1
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-01 13:43:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,14 +11,14 @@ from plugins.core.bypass_checker import app as Client2
 # Configuration
 id_pattern = re.compile(r"^.\d+$")
 
-BOT_TOKEN = os.environ.get("TOKEN", "your-bot-token")
-API_ID = int(os.environ.get("API_ID", "your-api-id"))
-API_HASH = os.environ.get("API_HASH", "your-api-hash")
+BOT_TOKEN = os.environ.get("TOKEN", "7713324413:AAGYzSbK5Z9KbhaJGiQdA7Rs-YaYLk4fNiU")
+API_ID = int(os.environ.get("API_ID", 11973721))
+API_HASH = os.environ.get("API_HASH", "5264bf4663e9159565603522f58d3c18")
 BOT_UPTIME = time.time()
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001821439025"))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1391556668 1242556540').split()]
-STRING_SESSION = os.environ.get("STRING_SESSION", "your-string-session")
+STRING_SESSION = os.environ.get("STRING_SESSION", "BQGC3RAANsUaEkcicYxlinT7b-sZqSEmmB3k0U5ejPI11DfFNZWgw95JzOZzClAtOggpEERj6Uw7_Vc4QfYaOZEm9YovvszyJzdZOyrkhgYbE2W4LhtoGkIxh184OswP_atDNQIXEDPzV_8mYtc-9JlilUumlfIDpd-YwSRWYPefy2Yvdvs00q7b5UuMPlVG_psmZWr7Plwp2Z3jscZ6ZoltifWu4MbIvODdxvMMTOjRUNOLHgnlGxanFAiBQn0vD7e8rceLlGWXZ9nKvlQitBvIB4vbUBOIiAglexGoRJZxG0z1dSSBdRiO5jp7QG0vOiNcT-Y7JNaNi2MxwTWIjK6za76X7AAAAABS8Xg8AA")
 
 # Bot Class
 class Bot(Client):
