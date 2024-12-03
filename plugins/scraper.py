9edeaaa3001b88aac64204c26fc274eec8fc99eb
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-12-03 11:58:01 +0530

    Update scraper.py

--- plugins/scraper.py
+++ plugins/scraper.py
@@ -2,6 +2,7 @@ from cloudscraper import create_scraper
 from re import sub
 from bs4 import BeautifulSoup
 import re
+from requests_html import AsyncHTMLSession
 
 async def tamilmv(url):
     cget = create_scraper().request
@@ -9,14 +10,30 @@ async def tamilmv(url):
     soup = BeautifulSoup(resp.text, "html.parser")
     mag = soup.select('a[href^="magnet:?xt=urn:btih:"]')
     tor = soup.select('a[data-fileext="torrent"]')
-    parse_data = f"<b><u>{soup.title.string}</u></b>"
+
+    parse_data = f"<b><u>{soup.title.string}</u></b>\n"
+    messages = []  # To store the parts of the message
+    max_length = 4096  # Telegram character limit
+    current_part = parse_data  # Current message part
 
     for no, (t, m) in enumerate(zip(tor, mag), start=1):
         filename = re.sub(r"www\S+|\- |\.torrent", "", t.string)
-        parse_data += f"""
+        entry = f"""
 <b>{no}.</b> <code>{filename}</code>
-<b>┖ Links: <a href="https://t.me/share/url?url={m['href']}">Magnet 🧲</a> | <a href="{t['href']}">Torrent 🌐</a></b>"""
-    return parse_data
+<b>┖ Links: <a href="https://t.me/share/url?url={m}">Magnet 🧲</a> | <a href="{t['href']}">Torrent 🌐</a></b>"""
+
+        # Check if adding this entry exceeds the max length
+        if len(current_part) + len(entry) > max_length:
+            messages.append(current_part)  # Save the current part
+            current_part = parse_data + entry  # Start a new part
+        else:
+            current_part += entry  # Append to the current part
+
+    # Append the last part if it contains data
+    if current_part:
+        messages.append(current_part)
+
+    return messages  # Return the list of message parts
     
 async def tamilmv1(url):
     cget = create_scraper().request
